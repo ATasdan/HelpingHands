378184945c28fe532f171333eea155c032e2babb
Hassam <45217889+PawnsApprentice@users.noreply.github.com>
2022-05-02 22:45:41 +0300

    functionality to map with distances and lines

--- frontend/screens/Askforblood/AskBloodTwo.js
+++ frontend/screens/Askforblood/AskBloodTwo.js
@@ -19,7 +19,8 @@ import MapViewDirections from "react-native-maps-directions";
 
 import { Picker } from "@react-native-picker/picker";
 import { api } from "../../api/api";
-
+import LoadingAnim from "../../components/LoadingAnim";
+import { GOOGLE_API, MAPS_API } from "@env";
 const BloodTwo = (props) => {
   const { userName, userPhone, userDate, selectedGroup, userGroup, userUnits } =
     props.route.params;
@@ -29,31 +30,36 @@ const BloodTwo = (props) => {
 
   const [hospResults, setHospResults] = useState([]);
   const [locPerm, setLocPerm] = useState(false);
-  const [latitude, setLatitude] = useState(null); //39.872102
-  const [longitude, setLongitude] = useState(null); //32.748339
-
-  const [pickerVal, setPickerVal] = useState();
+  const [latitude, setLatitude] = useState(0); //39.872102
+  const [longitude, setLongitude] = useState(0); //32.748339
+  const [loading, setLoading] = useState(false); // for spinner
+  const [pickerVal, setPickerVal] = useState("Select Hospitals");
   const [latitudeMarker, setLatitudeMarker] = useState(0);
   const [longitudeMarker, setLongitudeMarker] = useState(0);
+  const [distance, setDistance] = useState("");
+  const [duration, setDuration] = useState("");
 
   useEffect(() => {
     if (latitude === null || longitude === null) {
       handleHospitalSearchTemp();
     }
-    if (hospResults.length === 0) {
-      handleHospitalSearchTemp();
-    }
+
     (async () => {
       try {
         const { status } = await Location.requestForegroundPermissionsAsync();
+
         if (status === "granted") {
+          setLoading(true);
           let location = await Location.getCurrentPositionAsync({
             accuracy: Location.Accuracy.Highest,
             maximumAge: 10000,
           });
+          setLoading(false);
           setLocPerm(true);
           setLatitude(location.coords.latitude);
           setLongitude(location.coords.longitude);
+          setLatitudeMarker(latitude);
+          setLongitudeMarker(longitude);
 
           // handleRestaurantSearch();
         } else {
@@ -67,23 +73,29 @@ const BloodTwo = (props) => {
   }, []);
 
   const createRequest = async () => {
-    try {
-      console.log(
-        typeof latitudeMarker,
-        typeof longitudeMarker,
-        selectedGroup,
-        pickerVal,
-        userDate
-      );
-      const response = await api.post("/bloodRequest/create", {
-        latitude: latitudeMarker,
-        longitude: longitudeMarker,
-        bloodType: selectedGroup,
-        hospital: pickerVal,
-        expDate: userDate,
-      });
-    } catch (error) {
-      console.log(error.response.data);
+    if (
+      latitudeMarker === 0 ||
+      longitudeMarker === 0 ||
+      pickerVal === "Select Hospitals"
+    ) {
+      alert("Please select a hospital!");
+    } else {
+      try {
+        const response = await api.post("/bloodRequest/create", {
+          latitude: latitudeMarker,
+          longitude: longitudeMarker,
+          bloodType: selectedGroup,
+          hospital: pickerVal,
+          expDate: userDate,
+        });
+        navigation.navigate("Home", {
+          usrName: userName,
+          usrPhone: userPhone,
+          bloodType: userGroup,
+        });
+      } catch (error) {
+        console.log(error.response.data);
+      }
     }
   };
 
@@ -117,7 +129,7 @@ const BloodTwo = (props) => {
     const location = `location=${latitude},${longitude}`;
     const radius = "&radius=10000";
     const type = "&keyword=hospital";
-    //const key = ;
+    const key = GOOGLE_API;
 
     const hospitalSearchUrl = url + location + radius + type + key;
 
@@ -160,6 +172,28 @@ const BloodTwo = (props) => {
     //cleanHospitalData();
   };
 
+  const distanceCalculator = async () => {
+    //calls distance api here
+    const url = "https://maps.googleapis.com/maps/api/distancematrix/json?";
+    const origins = `origins=${latitude},${longitude}`;
+    const destinations = `&destinations=${latitudeMarker},${longitudeMarker}`;
+    const key = GOOGLE_API;
+
+    const distanceUrl = url + origins + destinations + key;
+
+    await fetch(distanceUrl)
+      .then((res) => res.json())
+      .then((res) => {
+        for (let googlePlace of res.rows) {
+          for (let elements of googlePlace.elements) {
+            setDistance(elements.distance.text);
+            setDuration(elements.duration.text);
+          }
+        }
+      })
+      .catch((e) => console.log(e));
+  };
+
   const handleHospitalSearch = async () => {
     if (locPerm === false) {
       getLocationAsync();
@@ -168,10 +202,10 @@ const BloodTwo = (props) => {
     const location = `location=${latitude},${longitude}`;
     const radius = "&radius=10000";
     const type = "&keyword=hospital";
-    //const key = ;
+    const key = GOOGLE_API;
 
     const hospitalSearchUrl = url + location + radius + type + key;
-
+    console.log(hospitalSearchUrl);
     await fetch(hospitalSearchUrl)
       .then((res) => res.json())
       .then((res) => {
@@ -226,6 +260,7 @@ const BloodTwo = (props) => {
 
   return (
     <SafeAreaView style={styles.container}>
+      <LoadingAnim isLoading={loading}></LoadingAnim>
       <View style={styles.logoContainer}>
         <Text style={styles.text}>Get Well Soon!</Text>
         <Text style={{ marginTop: 10, color: "grey" }}>
@@ -234,6 +269,10 @@ const BloodTwo = (props) => {
       </View>
 
       <View style={styles.footerContainer}>
+        <Text></Text>
+        <Text style={{ marginTop: 10, color: "#FF69B4", fontWeight: "bold" }}>
+          Distance: {distance}, Duration: {duration}
+        </Text>
         <TouchableOpacity onPress={viewHospitals} style={styles.secondaryBtn}>
           <Text style={{ color: Theme.white, fontWeight: "bold" }}>
             View Nearby Hospitals
@@ -249,78 +288,92 @@ const BloodTwo = (props) => {
         <></>
       )}
       {show === false ? (
-        <View style={styles.bodyContainer}>
-          <Text style={{ fontSize: Theme.font.medium }}>Please Choose:</Text>
+        <>
+          <View style={styles.bodyContainer}>
+            <Text style={{ fontSize: Theme.font.medium }}>Please Choose:</Text>
 
-          <View style={styles.inputContainer}>
-            <Picker
-              selectedValue={pickerVal}
-              onValueChange={(itemValue, itemIndex) => {
-                hospResults.map((objects) => {
-                  if (objects.placeName.toString() === itemValue.toString()) {
-                    setLatitudeMarker(objects.coordinate.latitude);
-                    setLongitudeMarker(objects.coordinate.longitude);
-                  }
-                }),
-                  setPickerVal(itemValue);
+            <View style={styles.inputContainer}>
+              <Picker
+                selectedValue={pickerVal}
+                onValueChange={(itemValue, itemIndex) => {
+                  hospResults.map((objects) => {
+                    if (objects.placeName.toString() === itemValue.toString()) {
+                      setLatitudeMarker(objects.coordinate.latitude);
+                      setLongitudeMarker(objects.coordinate.longitude);
+                    }
+                  }),
+                    setPickerVal(itemValue);
+
+                  setLoading(true);
+                  distanceCalculator();
+
+                  setLoading(false);
+                }}
+                dropdownIconColor="black"
+                mode="dropdown" // Android only */}
+              >
+                <Picker.Item
+                  label="Select Hospitals"
+                  value="Select Hospitals"
+                />
+                {hospResults.map((objects) => {
+                  return (
+                    <Picker.Item
+                      label={objects.placeName}
+                      value={objects.placeName}
+                      key={objects.placeId}
+                    />
+                  );
+                })}
+              </Picker>
+            </View>
+
+            <View style={styles.divider} />
+            <MapView
+              style={{ flex: 1 }}
+              provider={PROVIDER_GOOGLE}
+              showsUserLocation
+              initialRegion={{
+                latitude: 39.872102,
+                longitude: 32.748339,
+                latitudeDelta: 0.5,
+                longitudeDelta: 0.5,
               }}
-              dropdownIconColor="black"
-              mode="dropdown" // Android only */}
             >
-              <Picker.Item label="Select Hospitals" value="Unknown" />
-              {hospResults.map((objects) => {
-                return (
-                  <Picker.Item
-                    label={objects.placeName}
-                    value={objects.placeName}
-                    key={objects.placeId}
-                  />
-                );
-              })}
-            </Picker>
+              <MapViewDirections
+                origin={{ latitude: latitude, longitude: longitude }}
+                destination={{
+                  latitude: latitudeMarker,
+                  longitude: longitudeMarker,
+                }}
+                apikey={MAPS_API}
+                strokeWidth={3}
+                strokeColor="hotpink"
+              />
+              <Marker
+                coordinate={{
+                  latitude: latitudeMarker,
+                  longitude: longitudeMarker,
+                }}
+              />
+            </MapView>
           </View>
-
-          <View style={styles.divider} />
-          <MapView
-            style={{ flex: 1 }}
-            provider={PROVIDER_GOOGLE}
-            showsUserLocation
-            initialRegion={{
-              latitude: 39.872102,
-              longitude: 32.748339,
-              latitudeDelta: 0.5,
-              longitudeDelta: 0.5,
-            }}
-          >
-            <Marker
-              coordinate={{
-                latitude: latitudeMarker,
-                longitude: longitudeMarker,
+          <View style={styles.footerContainer}>
+            <TouchableOpacity
+              onPress={() => {
+                createRequest();
               }}
-            />
-          </MapView>
-        </View>
+              style={styles.mainbtn}
+            >
+              <Text style={{ color: Theme.white, fontWeight: "bold" }}>
+                Proceed
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </>
       ) : (
         <></>
       )}
-
-      <View style={styles.footerContainer}>
-        <TouchableOpacity
-          onPress={() => {
-            createRequest();
-            navigation.navigate("Home", {
-              usrName: userName,
-              usrPhone: userPhone,
-              bloodType: userGroup,
-            });
-          }}
-          style={styles.mainbtn}
-        >
-          <Text style={{ color: Theme.white, fontWeight: "bold" }}>
-            Proceed
-          </Text>
-        </TouchableOpacity>
-      </View>
     </SafeAreaView>
   );
 };
@@ -337,10 +390,11 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   logoContainer: {
-    flex: 0.3,
+    flex: 0.15,
     justifyContent: "center",
     paddingHorizontal: 20,
   },
+
   bodyContainer: {
     flex: 0.5,
     paddingHorizontal: 20,
@@ -357,6 +411,7 @@ const styles = StyleSheet.create({
   },
   text: {
     fontSize: 24,
+    marginTop: 50,
   },
   inputContainer: {
     width: "100%",
